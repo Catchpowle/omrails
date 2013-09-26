1aef6dfa6b2cfd2aa9e88dc9d6f07aa58e369320
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2013-09-27 00:35:09 +0100

    Add Image Uploading via URL

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
 
   validates :description, presence: true
   validates :user_id, presence: true
@@ -10,6 +10,10 @@ class Pin < ActiveRecord::Base
 
   Paperclip.options[:command_path] = 'C:\Program Files (x86)\ImageMagick-6.8.6-Q16'
   belongs_to :user
+
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  end
   
   
 end
